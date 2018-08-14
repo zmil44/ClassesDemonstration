44571a917691fd15683a2ab77dbf5f34a95ef46f
zmil44 <zachary.j.miller@outlook.com>
2018-08-14 12:31:15 -0400

    initial commit

---
+++ Assignemnt 5.2/DemoScouts.cs
@@ -0,0 +1,35 @@
+﻿/*Zachary Miller
+ * 12/6/2017
+ * assignment 5.2
+ * The purpose of this program is to create two instances of the girl scout class and display the name, troop number, and dues owed
+ * DemoScouts.cs
+ * Bellevue University
+ */
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt_5._2
+{
+    class DemoScouts
+    {
+        static void Main(string[] args)
+        {
+            GirlScout girl1 = new GirlScout();
+            GirlScout girl2 = new GirlScout();
+            girl1.SetScoutName("Kelly");
+            girl1.SetTroopNumber(66);
+            girl1.SetDues(125.50);
+            girl2.SetScoutName();
+            girl2.SetTroopNumber();
+            girl2.SetDues();
+
+            Console.WriteLine(GirlScout.motto);
+            Console.WriteLine("{0} of Troop number {1} owes ${2} in dues.", girl1.GetName(), girl1.GetTroop(), girl1.GetDues());
+            Console.WriteLine("{0} of Troop number {1} owes ${2} in dues.", girl2.GetName(), girl2.GetTroop(), girl2.GetDues());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignemnt 5.2/GirlScout.cs
@@ -0,0 +1,67 @@
+﻿/*Zachary Miller
+ * 12/6/2017
+ * assignment 5.2
+ * The purpose of this program is to set and return the values of dues owed, name, and troop number
+ * GirlScout.cs
+ * Bellevue University
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignemnt_5._2
+{
+    class GirlScout
+    {
+        public const string motto = "To obey the Girl Scout Law.";
+        public string name;
+        public int troopNumber;
+        public double duesOwed;
+
+        public void SetScoutName()//default name
+        {
+            name = "default";
+        }
+
+        public void SetScoutName(String value)//set name of the girl scout
+        {
+            name = value;
+        }
+
+        public string GetName()//return the girl scouts name
+        {
+            return name;
+        }
+        public void SetTroopNumber()//default troop number
+        {
+            troopNumber = 0;
+        }
+
+        public void SetTroopNumber(int value)//set troop number
+        {
+            troopNumber = value;
+        }
+
+        public int GetTroop()//return troop number
+        {
+            return troopNumber;
+        }
+        
+        public void SetDues()//default dues
+        {
+            duesOwed = 0.0;
+        }
+
+        public void SetDues(double value)//set dues owed
+        {
+            duesOwed = value;
+        }
+        public double GetDues()//return the amount of dues owed
+        {
+            return duesOwed;
+        }
+        
+    }
+}
